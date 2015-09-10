bbb64e5af6303a4101acabeff0b9fda57a518839
Usuario <Usuario@Usuario-PC>
2015-09-10 09:54:13 -0300

    Gerador de Pontuação

--- bolaoweb/src/main/java/bolao/controller/GeradordePontuacao.java
+++ bolaoweb/src/main/java/bolao/controller/GeradordePontuacao.java
@@ -7,6 +7,9 @@ package bolao.controller;
 
 import bolaoweb.model.Palpite;
 import bolaoweb.model.Partidas;
+import bolaoweb.modelDAO.PartidasDAO;
+import java.text.ParseException;
+import java.util.List;
 
 /**
  *
@@ -15,9 +18,17 @@ import bolaoweb.model.Partidas;
 public class GeradordePontuacao {
     private Partidas partida;
     private Palpite palpite;
+    private List <Palpite> listaPalpite;
+    private List <Partidas> listaPartida;
+    private PalpiteBEAN palpiteBEAN;
+    private PartidasBEAN partidaBEAN;
+    private String filtro;
+    private PartidasDAO partidasDAO;
     
-    public void validarpalpites(String filtro){
-        
+    public void gerarPontuacao() throws ParseException{
+        partidasDAO.setFiltro(filtro);
+        partidasDAO.setTipoFiltro("dataPartida");
+        listaPartida = partidasDAO.getLista();
     
     }
     
